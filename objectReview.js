b6ab0cd90acb17c7492f671807f223c88367e8c4
Mai Nguyen <mainguyen.vn91@yahoo.com>
2019-05-09 15:13:27 +0200

    Solved this file

--- objectReview.js
+++ objectReview.js
@@ -1,19 +1,30 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
-
+//Your code here
+let favoriteThings = {
+  band: 'None',
+  food: 'Broccoli',
+  person: 'Myself',
+  book: 'The Hundred-Year-Old Man Who Climbed Out the Window and Disappeared',
+  movie: 'The Man from U.N.C.L.E.',
+  holiday: 'My birthday'
+}
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
-
+//Your code here
+favoriteThings.car = 'None';
+favoriteThings.brand = 'Organic and ethical';
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+//Your code here
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades of Gray';
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
-
+//Your code here
+alert(favoriteThings.person);
+alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -21,26 +32,38 @@
 
 
 var user = {
-    name: 'Tyler McGinnis', 
-    email: null,
-    pwHash: 'U+Ldlngx2BYQk',
-    birthday: undefined,
-    username: 'tylermcginnis33',
-    age: 0
+  name: 'Tyler McGinnis',
+  email: null,
+  pwHash: 'U+Ldlngx2BYQk',
+  birthday: undefined,
+  username: 'tylermcginnis33',
+  age: 0
 }
 
 //Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
+//Your code here
+function removeFalsyValues(obj) {
+  for (var value in obj) {
+    if (!obj[value]) {
+      delete obj[value];
+    }
+  }
+  return obj;
+}
 
+removeFalsyValues(user)
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Your code here
+//Your code here
+user.name = 'Mai';
+user.pwHash = 'acb123';
+user.username = 'Mai123';
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
-
+//Your code here
+console.log(user);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -49,15 +72,23 @@ var user = {
 
 //create an empty object called methodCollection. 
 
-  //Your code here
+//Your code here
+let methodCollection = {};
+//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console.
 
-//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
-
-  //Your code here
+//Your code here
+methodCollection.alertHello = function () {
+  alert('hello');
+}
+methodCollection.logHello = function () {
+  console.log('hello');
+}
 
 //Now call your alertHello and logHello methods. 
 
-  //Your code here
+//Your code here
+methodCollection.alertHello();
+console.log(methodCollection.logHello());
 
 
 
@@ -67,6 +98,23 @@ var user = {
 
 //write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
+function voweler(str) {
+  var splitArr = str.toLowerCase().split("");
+  var obj = {};
+  var vowels = "aeiou";
 
+  splitArr.forEach(function (letter) {
+    if (vowels.indexOf(letter) !== -1) {
+      if (obj[letter]) {
+        obj[letter]++;
+      } else {
+        obj[letter] = 1;
+      }
+    }
+  });
+  console.log(obj);
+}
+
+voweler('This is a test')
 
 
